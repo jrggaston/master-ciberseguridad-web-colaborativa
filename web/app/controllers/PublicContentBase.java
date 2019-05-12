3d86490b52732466db60f95d7e03c711f281e19b
Jorge <gastonpuig@gmail.com>
2019-05-12 20:27:48 +0200

    registration username check
    The registration function shall not allow to register a user with an existing username.

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,9 +12,22 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+		
+		User u_check = User.loadUser(username)
+		if (u_check == null)
+		{
+			User u = new User(username, HashUtils.getMd5(password), type, -1);
+			u.save();
+			registerComplete();
+		}
+		else	
+		{
+				registerFailure();
+		}
+    }
+	
+	public static void registerFailure(){
+        render();
     }
 
     public static void registerComplete(){
